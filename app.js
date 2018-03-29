f93295fadb51182cca77c50e7c6df966f9917535
Lalit Kumar <lalitksharma.com>
2018-03-30 00:05:57 +0800

    third test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 app.get('/',function(req,res) {
   res.send("<h1>hi there, Hello from NodeJS World!<p>test</p>");
   res.send("<h2>last manual test was successful!"</h2>");
+  res.send("<h3>3rd test</h3>");
 });
 
 app.listen(4000);
