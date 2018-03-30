420eae71569e65dce250e1b32b40ac4723176837
Lalit Kumar <lalitksharma.com>
2018-03-30 20:10:37 +0800

    added dockefile

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("<h1>hi there, Hello from NodeJS World!<p>test</p>");
-  res.send("<h2>last manual test was successful!</h2>");
-  res.send("<h3>4th test</h3>");
+  res.send("<h1>hi there, Hello from NodeJS World!");
+  //res.send("<h2>last manual test was successful!</h2>");
+  //res.send("<h3>4th test</h3>");
 });
 
-app.listen(4000);
+app.listen(8000);
+console.log("Server listening at http://127.0.0.1:8000/");
+
 
